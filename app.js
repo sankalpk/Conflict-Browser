42ba263049c8ed9789dab9df3d03a88680aec7e4
Sankalp Kulshreshtha <sankalp221@gmail.com>
2013-02-22 17:07:43 -0500

    Added coordinates, libraries, and disputes restful methods

--- app.js
+++ app.js
@@ -11,8 +11,9 @@ var fs = require("fs");
 // body of a request
 app.use(express.bodyParser());
 
-// The global datastore for this example
-var datastore;
+var disputes;
+var coordinates;
+var libraries; //Hash of usernames as keys and an array of dispute id as value
 
 // Asynchronously read file contents, then call callbackFn
 function readFile(filename, defaultData, callbackFn) {
@@ -38,6 +39,111 @@ function writeFile(filename, data, callbackFn) {
     if (callbackFn) callbackFn(err);
   });
 }
+
+//DISPUTES
+app.get("/disputes",function(request, response){
+  response.send({
+    disputes: disputes,
+    success: true
+  });
+});
+
+app.get("/disputes/:id",function(request, response){
+  var id=request.params.id;
+  response.send({
+    dispute:disputes["\""+id+"\""],
+    success: true
+  });
+});
+
+
+
+//LIBRARIES
+app.get("/libraries",function(request, response){ //get all libraries
+  response.send({
+    libraries: libraries,
+    success: true
+  });
+});
+
+app.get("/libraries/:name",function(request,response){ //get one library by name
+  var name=request.params.name;
+  response.send({
+    library: libraries[name],
+    success:true
+  });
+});
+
+app.post("/libraries", function(request, response) { //create a new library associated with "name"
+  libraries[request.body.name]={
+                  "disputes": [],
+                  "description": request.body.description,
+                  "date_modified": new Date(),
+                  "date_added": new Date(),
+                  "time_start": -1,
+                  "time_end": -1
+                  };
+  writeFile("libraries.json", JSON.stringify(libraries));
+  response.send({
+    library: libraries[request.body.name],
+    success: true
+  });
+});
+
+app.put("/libraries/:name", function(request, response){//updates user library with new array of disputes
+  var name=request.params.name;
+  var oldItem=libraries[name];
+  var library={ "disputes": JSON.parse(request.body.disputes), //an array of dispute ids
+                "description": request.body.description,
+                "date_modified": new Date(),
+                "time_start": -1, //TODO write a function to calculate and change
+                "time_end": -1
+              };
+  library.disputes = (library.disputes !== undefined) ? library.disputes : oldItem.disputes;
+  library.description = (library.description !== undefined) ? library.description : oldItem.description;
+  libraries[name]= library;
+  writeFile("libraries.json", JSON.stringify(libraries));
+
+  response.send({
+    library: library,
+    success: true
+  });
+});
+
+app.delete("/libraries", function(request, response){ //deletes all libraries
+  libraries={};
+  writeFile("libraries.json", JSON.stringify(libraries));
+  response.send({
+    libraries: libraries,
+    success: true
+  });
+});
+
+app.delete("libraries/:name", function(request, response){ //deletes a library by name
+  var name=request.params.name;
+  var id=request.params.id;
+
+
+})
+
+
+//COORDINATES
+app.get("/coordinates",function(){
+  response.send({
+    coordinates: coordinates,
+    success: true
+  });
+});
+
+app.get("/coordinates/:ccode", function(){
+  var ccode=request.params.ccode; //by ISO3 country code, i.e. United Staes is USA
+  response.send({
+    coordinate: coordinates[ccode],
+    success: true
+  });
+})
+
+
 
 
 // This is for serving files in the static directory
@@ -68,9 +174,15 @@ app.get("/static/data/:staticFilename", function (request, response) {
 
 function initServer() {
   // When we start the server, we must load the stored data
-  var defaultList = "[]";
-  readFile("static/data/data.json", defaultList, function(err, data) {
-    todoList = JSON.parse(data);
+  var defaultHash = "{}";
+  readFile("static/data/disputes.json", defaultHash, function(err, data) {
+    disputes = JSON.parse(data);
+  });
+  readFile("static/data/coordinates.json", defaultHash, function(err, data) {
+    coordinates = JSON.parse(data);
+  });
+  readFile("static/data/libraries.json", defaultHash, function(err, data){
+    libraries=JSON.parse(data);
   });
 }
 
